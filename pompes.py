fb65faa07e03f53e506f52519237fe2f7e755119
Ludens509 <alexandreludens2@gmail.com>
2022-08-18 14:31:58 -0400

    second commit

--- pompes.py
+++ pompes.py
@@ -15,19 +15,19 @@ while (b1):
         case 'a':
           nom=''
           
-          while(nom != "Lalue" or nom !="Tabarre" or nom !="Clercine" or nom !="Petion-ville"):
+          while nom != "Lalue" or nom != "Tabarre" or nom != "Clercine" or nom != "Petion-ville":
             nom =input("Entrer le nom de la zones( Lalue, Tabarre, Clercine et Pétion-ville): ")
           
           capacite_gazoline=0
-          while(capacite_gazoline <= 0 ):
+          while capacite_gazoline <= 0:
             capacite_gazoline= int(input("Entrer la capacite en Gazoline"))
 
             capacite_diesel=0
-          while(capacite_diesel < 0 ):  
+          while capacite_diesel < 0:
             capacite_diesel= int(input("Entrer la capacite en Diesel"))
 
           qte_gallon_dispo = 0
-          while(qte_gallon_dispo <=0  and qte_gallon_dispo>capacite_gazoline):
+          while qte_gallon_dispo <=0  and qte_gallon_dispo>capacite_gazoline:
             qte_gallon_dispo= input("Entrer la quantite de gallons disponible ")
 
           station = Station(nom,capacite_gazoline,pourcentage_gazoline,pourcentage_diesel,capacite_diesel)
--- stations.py
+++ stations.py
@@ -1,38 +1,35 @@
-
-
 class Station():
+    lalue, tabarre, clercine, petion_ville = dict(), dict(), dict(), dict()
 
-  lalue,tabarre,clercine,petion_ville = dict(), dict(), dict(), dict()
+    def __init__(self, nom, capacite_gazoline, pourcentage_gazoline, pourcentage_diesel, capacite_diesel):
+        self.nom = nom
+        self.capacite_gazoline = capacite_gazoline
+        self.capacite_diesel = capacite_diesel
+        self.pourcentage_diesel = pourcentage_diesel
+        self.pourcentage_gazoline = pourcentage_gazoline
 
-  def __init__(self,nom,capacite_gazoline,pourcentage_gazoline,pourcentage_diesel,capacite_diesel):
-    self.nom = nom
-    self.capacite_gazoline = capacite_gazoline
-    self.capacite_diesel = capacite_diesel
-    self.pourcentage_diesel = pourcentage_diesel
-    self.pourcentage_gazoline = pourcentage_gazoline
+        def getNom():
+            return self.nom
 
-    def getNom():
-      return self.nom
+        def getCapaciteGazoline():
+            return self.capacite_gazoline
 
-    def getCapaciteGazoline():
-      return self.capacite_gazoline
+        def setCapaciteGazoline(newCapacite_gazoline):
+            self.capacite_gazoline = newCapacite_gazoline
 
-    def setCapaciteGazoline(newCapacite_gazoline):
-      self.capacite_gazoline = newCapacite_gazoline  
+        def getCapaciteDisesel():
+            return self.capacite_diesel
 
-    def getCapaciteDisesel():
-      return self.capacite_diesel
+        def setCapaciteDiesel(newCapacite_diesel):
+            self.capacite_diesel = newCapacite_diesel
 
-    def setCapaciteDiesel(newCapacite_diesel):
-      self.capacite_diesel = newCapacite_diesel
+        def getPourcentageGazoline():
+            return self.pourcentage_gazoline
 
-    def getPourcentageGazoline():
-      return self.pourcentage_gazoline  
+        def getPourcentageDiesel():
+            return self.pourcentage_gazoline
 
-    def getPourcentageDiesel():
-      return self.pourcentage_gazoline
-
-    def enregistrer(self, nom):
-      Station.lalue.update({"nom":self.nom, "capacite Gazoline":self.capacite_gazoline,"capacite Diesel":self.capacite_diesel,"% Diesel":self.pourcentage_diesel,"%tage Gazoline":self.pourcentage_gazoline})
-      
-    
+        def enregistrer(self, nom):
+            Station.lalue.update \
+                ({"nom": self.nom, "capacite Gazoline": self.capacite_gazoline, "capacite Diesel": self.capacite_diesel
+                     , "% Diesel": self.pourcentage_diesel, "%tage Gazoline": self.pourcentage_gazoline})
